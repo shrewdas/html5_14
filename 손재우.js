c7683e63caff412cab0ca6673a9029a482eaf4c6
shrewdas <sonjw0116@naver.com>
2024-12-02 15:31:01 +0900

    손재우

---
+++ 손재우.js
@@ -0,0 +1,9 @@
+<script>
+    let selectBox = document.getElementById("selectBox");
+    let resultDiv = document.getElementById("result");
+
+    selectBox.addEventListener("change", function() {
+        let select = selectBox.options[selectBox.selectBox.selectedIndex].value;
+        resultDivinnerText = "좋아하는 과일은: " + select;
+    });
+</script>
